38fe6463025ea0ccdae14e823853cd420cfb67b7
Ryan Cheley <rcheley@gmail.com>
2016-11-22 19:40:37 -0800

    Code Updated for Post
    removed the function grey_color_func that wasn’t being used, updated
    the stopwords, and removed commented lines from final

--- Word Cloud from Twitter.py
+++ Word Cloud from Twitter.py
@@ -6,9 +6,7 @@ from tweepy import OAuthHandler
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 from scipy.misc import imread
-
-def grey_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
-    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+import time
 
 consumer_key = consumer_key
 consumer_secret = consumer_secret
@@ -26,6 +24,8 @@ for status in api.user_timeline():
     f.write(api.get_status(status.id).text.encode("utf-8"))
 f.close()
 
+
+
 words=' '
 count =0
 f = open('tweets', 'rb')
@@ -33,14 +33,13 @@ for line in f:
     words= words + line.decode("utf-8")
 f.close
 
-stopwords = {'https'}
+stopwords = {'https', 'co', 'RT'}
 
 logomask = imread('twitter_mask.png')
 
 wordcloud = WordCloud(
     font_path='/Users/Ryan/Library/Fonts/Inconsolata.otf',
     stopwords=STOPWORDS.union(stopwords),
-    #background_color='black',
     background_color='white',
     mask = logomask,
     max_words=500,
@@ -48,10 +47,7 @@ wordcloud = WordCloud(
     height=1400
 ).generate(words)
 
-#plt.imshow(wordcloud.recolor(color_func=grey_color_func, random_state=3))
 plt.imshow(wordcloud.recolor(color_func=None, random_state=3))
 plt.axis('off')
-plt.savefig('./tweetcloud2.png', dpi=300)
+plt.savefig('./Twitter Word Cloud - '+time.strftime("%Y%m%d")+'.png', dpi=300)
 plt.show()
-
-
